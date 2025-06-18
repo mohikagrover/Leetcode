4bc26d8bc49d033eea26232c0dfe80c45bcc35f9
mohikagrover <130286830+mohikagrover@users.noreply.github.com>
2025-06-18 22:58:14 +0530

    Time: 23 ms (81.15%) | Memory: 59.2 MB (80.63%) - LeetSync

---
+++ 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[][] divideArray(int[] nums, int k) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        if (n % 3 != 0) return new int[0][0];
+
+        int[][] ans = new int[n / 3][3];
+        int idx = 0;
+
+        for (int i = 0; i < n; i += 3) {
+            if (nums[i + 2] - nums[i] > k) {
+                return new int[0][0]; // Not possible
+            }
+
+            ans[idx][0] = nums[i];
+            ans[idx][1] = nums[i + 1];
+            ans[idx][2] = nums[i + 2];
+            idx++;
+        }
+
+        return ans;
+    }
+}
